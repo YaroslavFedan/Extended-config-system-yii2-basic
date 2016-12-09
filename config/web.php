30bd07aabe3ccb1b09c0d43155f49625dc434f6b
Jaroslav Fedan <yaroslav.fd@gmail.com>
2016-12-09 14:15:46 +0200

    clear cookieValidationKey

--- config/web.php
+++ config/web.php
@@ -5,7 +5,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'Fpn1d6DB-RZnpQW9RbXJDtRbCB5exSXG',
+            'cookieValidationKey' => '',
         ],
         'user' => [
             'identityClass' => 'app\models\User',
